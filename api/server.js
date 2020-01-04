7151d154eecc25a8c54cc02861889a1a40a00e7f
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-01-04 13:52:16 -0700

    added user route to server.js

--- api/server.js
+++ api/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const helmet = require('helmet');
 
 const chefRoute = require('./chef_route/chef_route_router');
+const userRoute = require('./user_route/user_route_router');
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use("/api/chef", chefRoute);
+server.use("/api/user", userRoute);
 
 
 module.exports = server;
--- api/user_route/user_route_model.js
+++ api/user_route/user_route_model.js
@@ -3,7 +3,10 @@ const db = require('../../data/dbConfig');
 module.exports = {
     getRecipes,
     findByName,
-    createChef
+    createChef,
+    getInstructionsById,
+    getIngredientsById,
+    findRecipe
 };
 
 function getRecipes(){
@@ -19,4 +22,23 @@ function findByName(username){
 
 function createChef(chef){
     return db('chefs').insert({...chef}, "*");
+}
+
+function findRecipe(id){
+    return db.select("recipes.id","recipes.name","recipes.description","recipes.image_url","recipes.meal_type","chefs.name")
+        .from("recipes")
+        .join("chefs","chefs.id","recipes.chef_id")
+        .where("id",id);
+}
+
+function getInstructionsById(id){
+    return db("instructions")
+        .select("instruction","step_number")
+        .where("recipe_id",id);
+}
+
+function getIngredientsById(id){
+    return db("ingredients")
+        .select("name","quantity")
+        .where("recipe_id",id);
 }
