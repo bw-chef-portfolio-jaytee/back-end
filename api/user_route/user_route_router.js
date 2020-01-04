05a158491d9a2937babc9e8a138b2a471168bc94
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-01-04 14:05:26 -0700

    added route to get by recipe id

--- api/user_route/user_route_router.js
+++ api/user_route/user_route_router.js
@@ -12,6 +12,26 @@ router.get("/recipes",(req,res)=>{
         .catch(error=>{
             res.status(500).json({message:"error getting users", error:error});
         });
-})
+});
+
+router.get("/recipes/:id",(req,res)=>{
+    const recipeData = {};
+    userDb.findRecipe(req.params.id)
+        .then(data=>{
+            recipeData = {...recipeData, ...data};
+            return userDb.getIngredientsById(req.params.id)
+                .then(data=>{
+                    recipeData= {...recipeData, ...data};
+                    return userDb.getInstructionsById(req.params.id)
+                        .then(data=>{
+                            recipeData = {...recipeData, ...data};
+                            res.status(200).json(recipeData);
+                        })
+                })
+        })
+        .catch(error=>{
+            res.status(500).json({message:"error getting recipe", error:error});
+        })
+});
 
 module.exports = router;
