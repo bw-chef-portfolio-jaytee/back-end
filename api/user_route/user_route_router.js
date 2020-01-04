ea7f9caccc6e518ae4853daa9d6bd95ed3241c14
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-01-04 13:56:31 -0700

    added get request in users route to get all recipes

--- api/user_route/user_route_router.js
+++ api/user_route/user_route_router.js
@@ -1,5 +1,17 @@
 const express = require('express');
 
+const userDb = require('./user_route_model');
+
 const router = express.Router();
+
+router.get("/recipes",(req,res)=>{
+    userDb.getRecipes()
+        .then(data=>{
+            res.status(200).json(data);
+        })
+        .catch(error=>{
+            res.status(500).json({message:"error getting users", error:error});
+        });
+})
 
 module.exports = router;
