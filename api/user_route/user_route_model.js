7d2ae63c2a69e7fd7bb7dbc4f2343b53f8f075a0
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-01-04 13:31:20 -0700

    added getRecipes, FindByName, and createChef in user_route_model.js

--- api/user_route/user_route_model.js
+++ api/user_route/user_route_model.js
@@ -1,5 +1,22 @@
 const db = require('../../data/dbConfig');
 
 module.exports = {
-
+    getRecipes,
+    findByName,
+    createChef
 };
+
+function getRecipes(){
+    return db("recipes");
+}
+
+function findByName(username){
+    return db('users')
+        .select('*')
+        .where('name', username)
+        .first();
+}
+
+function createChef(chef){
+    return db('chefs').insert({...chef}, "*");
+}
