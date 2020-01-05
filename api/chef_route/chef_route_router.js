a0b35d0c4161fd51519282e709c3a7c0c93c3b7c
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-01-05 12:45:58 -0700

    added route to allow chef to get all of their recipes

--- api/chef_route/chef_route_router.js
+++ api/chef_route/chef_route_router.js
@@ -1,5 +1,17 @@
 const express = require('express');
 
+const chefDb = require('./chef_route_model');
+
 const router = express.Router();
+
+router.get("/recipes",(req,res)=>{
+    chefDb.getChefRecipes(req.token.id)
+        .then(data=>{
+            res.status(200).json(data);
+        })
+        .catch(error=>{
+            res.status(500).json({message:"error getting chef's recipes", error});
+        })
+})
 
 module.exports = router;
