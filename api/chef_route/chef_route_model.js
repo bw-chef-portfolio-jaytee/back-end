a6adbc65e0109540986bcd1875e39314915370c2
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-01-05 12:03:23 -0700

    added insert functions in chef_route_model.js

--- api/chef_route/chef_route_model.js
+++ api/chef_route/chef_route_model.js
@@ -1,5 +1,25 @@
 const db = require('../../data/dbConfig');
 
 module.exports = {
-
+    getChefRecipes,
+    addRecipe,
+    addInstructions,
+    addIngredients
 };
+
+function getChefRecipes(id){
+    return db("recipes")
+        .where("chef_id",id);
+}
+
+function addRecipe(data){
+    return db.insert({...data}).into("recipes");
+}
+
+function addInstructions(data){
+    return db.insert({...data}).into("instructions");
+}
+
+function addIngredients(data){
+    return db.insert({...data}).into("ingredients");
+}
