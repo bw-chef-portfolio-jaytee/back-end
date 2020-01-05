36beb2a65ae24e310032a1fe049594bda93d6df2
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-01-05 12:20:58 -0700

    created middleware to validate token and runs on every call in /api/chef

---
+++ api/middleware/ValidateToken.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken"); 
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  console.log(`checking for valid token`);
+
+  if (authorization) {
+    const secret = process.env.JWT_SECRET || "Chef has been signed ind";
+
+    jwt.verify(authorization, secret, function(error, decodedToken) {
+      if (error) {
+        res.status(401).json({ message: "Invalid Token" });
+      } else {
+        req.token = decodedToken;
+        
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "Please login and try again" });
+  }
+};
--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const validateToken = require('./middleware/ValidateToken');
+
 const chefRoute = require('./chef_route/chef_route_router');
 const userRoute = require('./user_route/user_route_router');
 
@@ -8,7 +10,7 @@ const server = express();
 
 server.use(express.json());
 server.use(helmet());
-server.use("/api/chef", chefRoute);
+server.use("/api/chef", validateToken, chefRoute);
 server.use("/api/user", userRoute);
 
 
