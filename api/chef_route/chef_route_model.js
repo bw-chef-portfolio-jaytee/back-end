43d998dba042adc45fe002e84c7e60f5fe3a2156
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-01-06 10:46:39 -0700

    edited chef_route_model and commented out all functions that use ingredients and instructions tables

--- api/chef_route/chef_route_model.js
+++ api/chef_route/chef_route_model.js
@@ -3,14 +3,14 @@ const db = require('../../data/dbConfig');
 module.exports = {
     getChefRecipes,
     addRecipe,
-    addInstructions,
-    addIngredients,
-    editIngredients,
-    editInstructions,
+    //addInstructions,
+    //addIngredients,
+    //editIngredients,
+    //editInstructions,
     editRecipe,
-    deleteIngredients,
+    //deleteIngredients,
     deleteRecipe,
-    deleteInstructions
+    //deleteInstructions
 };
 
 function getChefRecipes(id){
@@ -22,34 +22,34 @@ function addRecipe(data){
     return db.insert({...data}).into("recipes");
 }
 
-function addInstructions(data){
-    return db.insert({...data}).into("instructions");
-}
+// function addInstructions(data){
+//     return db.insert({...data}).into("instructions");
+// }
 
-function addIngredients(data){
-    return db.insert({...data}).into("ingredients");
-}
+// function addIngredients(data){
+//     return db.insert({...data}).into("ingredients");
+// }
 
 function editRecipe(data,id){
     return db("recipes").where("id",id).update(data);
 }
 
-function editIngredients(data,id){
-    return db("ingredients").where("id",id).update(data);
-}
+// function editIngredients(data,id){
+//     return db("ingredients").where("id",id).update(data);
+// }
 
-function editInstructions(data,id){
-    return db("instructions").where("id",id).update(data);
-}
+// function editInstructions(data,id){
+//     return db("instructions").where("id",id).update(data);
+// }
 
 function deleteRecipe(id){
     return db("recipes").where("id", id).del();
 }
 
-function deleteIngredients(id){
-    return db("recipes").where("id", id).del();
-}
+// function deleteIngredients(id){
+//     return db("recipes").where("id", id).del();
+// }
 
-function deleteInstructions(id){
-    return db("instructions").where("id", id).del();
-}
+// function deleteInstructions(id){
+//     return db("instructions").where("id", id).del();
+// }
