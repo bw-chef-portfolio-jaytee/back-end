c034761851af5013ac9177bd984c9bfff33bf5f1
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-01-06 10:41:49 -0700

    recreated database. there is no ingredients and instructions table but they are now text fields in the recipes table. commented out getInstructionsById and get IngredientsById in user_route_model.js

--- api/user_route/user_route_model.js
+++ api/user_route/user_route_model.js
@@ -4,13 +4,13 @@ module.exports = {
     getRecipes,
     findByName,
     createChef,
-    getInstructionsById,
-    getIngredientsById,
+    //getInstructionsById,
+    //getIngredientsById,
     findRecipe
 };
 
 function getRecipes(){
-    return db.select("recipes.id","recipes.name","recipes.description","recipes.image_url","recipes.meal_type","chefs.username")
+    return db.select("recipes.id","recipes.name","recipes.description","recipes.image_url","recipes.meal_type","recipes.ingredients","recipes.instructions","chefs.username")
         .from("recipes")
         .join("chefs","chefs.id","recipes.chef_id")
 }
@@ -27,19 +27,19 @@ function createChef(chef){
 }
 
 function findRecipe(id){
-    return db.select("recipes.id","recipes.name","recipes.description","recipes.image_url","recipes.meal_type","chefs.username")
+    return db.select("recipes.id","recipes.name","recipes.description","recipes.image_url","recipes.meal_type","recipes.ingredients","recipes.instructions","chefs.username")
         .from("recipes")
         .join("chefs","chefs.id","recipes.chef_id")
         .where("recipes.id",id)
         .first();
 }
 
-function getInstructionsById(id){
-    return db("instructions")
-        .where("recipe_id",id);
-}
+// function getInstructionsById(id){
+//     return db("instructions")
+//         .where("recipe_id",id);
+// }
 
-function getIngredientsById(id){
-    return db("ingredients")
-        .where("recipe_id",id);
-}
+// function getIngredientsById(id){
+//     return db("ingredients")
+//         .where("recipe_id",id);
+// }
