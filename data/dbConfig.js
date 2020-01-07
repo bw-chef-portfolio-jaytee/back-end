6af08904aa478bf00263c2753a0c42d7b072f197
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-01-07 11:04:38 -0700

    prepared postgres to be used on deployment

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
+
 const knex = require("knex");
-const knexfile = require("../knexfile.js");
+const config = require("../knexfile.js");
+const environment = process.env.DB_CONNECT || "development";
 
-const environment = process.env.DB_ENV || "development";
-
-module.exports = knex(knexfile[environment]);
+module.exports = knex(config[environment]);
