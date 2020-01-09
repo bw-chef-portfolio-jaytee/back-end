9a3ed42a6ca16787a8a6b396a728ade3feb43dbc
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-01-09 10:44:44 -0700

    hopefully fixed phone number validation

--- api/middleware/ValidatePhoneNumber.js
+++ api/middleware/ValidatePhoneNumber.js
@@ -2,7 +2,7 @@ module.exports = (req, res, next) => {
     if(req.body.phone_number){
         if(req.body.phone_number.trim() !== ""&& req.body.phone_number !== null){
             console.log("the string in not empty");
-        if(req.body.phone_number.split(" ").length !=2){
+        }else if(req.body.phone_number.split(" ").length !=2){
             res.status(400).json({message:"you have provided an invalid phone number."})
         }else if(!req.body.phone_number.includes("-")===10){
             res.status(400).json({message:"you have provided an invalid phone number."})
@@ -19,4 +19,4 @@ module.exports = (req, res, next) => {
         next();
     }
 
-  }
+    }
